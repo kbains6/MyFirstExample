2f93d3b48fe4b99de2ac790ef97c4926d3ab14c6
kbains6 <kbains6@mail.csuchico.edu>
2020-03-11 11:33:32 -0700

    added lint

--- main.cpp
+++ main.cpp
@@ -5,10 +5,13 @@ using std::cout;
 
 int main()
 {
-   
+  
+    int three;
+
     cout<<"Hi, please enter two numbers: ";
     int one, two;
-    cin >> one >> two;
+    cin>>one>>two;
+
 
     return 0;
 }
